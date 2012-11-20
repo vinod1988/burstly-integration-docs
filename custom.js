9c0910b5c2343e9d07ea7a533d9a7100df033f71
Tommy McGylnn <tommymcgylnn@Tommys-MacBook-Air.local>
2012-11-20 13:45:43 -0800

    Custom JS

---
+++ custom.js
@@ -0,0 +1,12 @@
+$(window).load(function () {
+
+    var _wikiBodyTop = $('#wiki-body').offset().top;
+    var _wikiHeader = $('#head');
+    var _wikiNavBar = $('#wiki-rightbar');
+    var _wikiBody = $('#wiki-body');
+
+    $(window).scroll(function() {
+		var scrollNavTo = Math.max(_wikiBodyTop, $(this).scrollTop());
+		_wikiNavBar.css('top', scrollNavTo + "px");
+	});
+});
