68d902aa64156e0c30a8b362aa6274e51dfee78c
Tommy McGylnn <tommymcgylnn@Tommys-MacBook-Air.local>
2012-11-20 15:58:39 -0800

    Custom JS

--- custom.js
+++ custom.js
@@ -5,10 +5,18 @@ $(window).load(function () {
     var _wikiBody = $('#wiki-body');
     var _wikiBodyTop = _wikiBody.offset().top;
     var _navMargin = 10;
+    var _sectionName = $('h1').html();
 
     $(window).scroll(function() {
 		var scrollNavTo = Math.max(_wikiBodyTop, $(this).scrollTop() + _navMargin);
 		var navOffset = _wikiNavBar.offset();
 		_wikiNavBar.offset({left: navOffset.left, top: scrollNavTo});
+	});
+
+	// Get page name
+
+	// Create nav content.
+	$('#wiki-body h2').each(function() {
+		_wikiNavBar.append($(this).html());
 	});
 });
