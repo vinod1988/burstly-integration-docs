b0856ed2f587f8e6c5bf25cdda19cdae6268e492
Tommy McGylnn <tommymcgylnn@Tommys-MacBook-Air.local>
2012-11-20 17:56:42 -0800

    JS

--- custom.js
+++ custom.js
@@ -1,5 +1,6 @@
 $(window).load(function () {
 
+	var _wikiWrapper = $('#wiki-wrapper');
     var _wikiHeader = $('#head');
     var _wikiNavBar = $('#wiki-rightbar');
     var _wikiBody = $('#wiki-body');
@@ -7,24 +8,37 @@ $(window).load(function () {
     var _navMargin = 10;
     var _sectionName = $('h1').text();
     var _navOffsetX = _wikiNavBar.offset().left;
+    var _navWidth = _wikiNavBar.innerWidth();
+
+    // Add a div after the nav bar that we will use for sizing the nav bar when it is in a fixed position.
+    _wikiNavBar.after('<div id="wiki-rightbar-sizer"></div>');
+    var _wikiNavBarSizer = $('#wiki-rightbar-sizer');
 
     $(window).scroll(function() {
     	var bodyTop = _wikiBodyTop - $(this).scrollTop();
     	if (bodyTop < 10)
     	{
     		// Make sure fix position is applied for nav bar.
-    		_wikiNavBar.addClass('fixed');
+    		if (!_wikiNavBar.hasClass('fixed'))
+			{
+				_wikiNavBar.addClass('fixed');
+				resizeFixedNav();
+			}
     	}
     	else
     	{
     		// Make sure relative position is applied for nav bar.
     		_wikiNavBar.removeClass('fixed');
     	}
-		//var scrollNavTo = Math.max(_wikiBodyTop, $(this).scrollTop() + _navMargin);
-		//_wikiNavBar.offset({left: _navOffsetX, top: scrollNavTo});
 	});
 
-	// Get page name
+	$(window).resize(function() {
+		// Determine if we need to manually set the width of the nav bar.
+		if (_wikiNavBar.hasClass('fixed'))
+		{
+			resizeFixedNav();
+		}
+	});
 
 	// Create nav content.
 	var _navHtml = '<ul>';
@@ -36,4 +50,9 @@ $(window).load(function () {
 	_navHtml += '</ul>';
 	_wikiNavBar.find('a:contains(\''+_sectionName+'\')').after(_navHtml);
 
+
+	function resizeFixedNav()
+	{
+		_wikiNavBar.width(_wikiNavBarSizer.width());
+	}
 });
