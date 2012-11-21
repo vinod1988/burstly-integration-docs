6fe62cbb636c90519ec9779baba46a28ce6c89d6
Tommy McGylnn <tommymcgylnn@new-host-3.home>
2012-11-21 15:15:59 -0800

    js

--- custom.js
+++ custom.js
@@ -9,6 +9,7 @@ $(window).load(function () {
     var _sectionName = $('h1').text();
     var _navOffsetX = _wikiNavBar.offset().left;
     var _sectionAnchor = _wikiNavBar.find('a:contains(\''+_sectionName+'\')');
+    var _currentSubSection = null;
 
     // Add class to all section anchors.
     _wikiNavBar.find('a').addClass('section');
@@ -20,8 +21,14 @@ $(window).load(function () {
     _wikiNavBar.after('<div id="wiki-rightbar-sizer" style="width: 25%;"></div>');
     var _wikiNavBarSizer = $('#wiki-rightbar-sizer');
 
+    // Create list of sub section elements.
+    // These will be used to determine which nav links should highlight based on scroll position.
+    var _subSectionHeaders = _wikiBody.find('h2');
+
     $(window).scroll(function() {
-    	var bodyTop = _wikiBodyTop - $(this).scrollTop();
+    	var windowScrollTop = $(this).scrollTop();
+    	var windowHeight = $(this).height();
+    	var bodyTop = _wikiBodyTop - windowScrollTop;
     	if (bodyTop < 10)
     	{
     		// Make sure fix position is applied for nav bar.
@@ -35,6 +42,42 @@ $(window).load(function () {
     	{
     		// Make sure relative position is applied for nav bar.
     		_wikiNavBar.removeClass('fixed');
+    	}
+
+    	// Determine which subsection we are scrolled to.
+    	var i = 0;
+    	var len = _subSectionHeaders.length;
+    	var currentSubSectionHeader = null;
+    	while (i < len)
+    	{
+    		var subSectionHeader = _subSectionHeaders[i];
+    		var posInWindow = subSectionHeader.offset().top - windowScrollTop;
+    		// Determine if the sub-section header is above the bottom third of the window.
+			if (posInWindow < windowHeight * 0.66)
+			{
+				currentSubSectionHeader = subSectionHeader;
+			}
+			else
+			{
+				i = len;
+			}
+
+    		i++;
+    	}
+
+    	// Set new sub-section header.
+    	// Determine if the value has changed.
+    	if (currentSubSectionHeader != _currentSubSection)
+    	{
+    		_currentSubSection = currentSubSectionHeader;
+    		var subSectionName = $(_currentSubSection).text();
+
+    		// Remove "current" class from all sub section links.
+			$(_wikiNavBar).find('li').each(function() {
+				$(this).removeClass('current');
+			});
+			// Add "current" class to corresponding sub section link.
+			_wikiNavBar.find('li:contains(\''+subSectionName+'\')').addClass('current');
     	}
 	});
 
@@ -54,7 +97,7 @@ $(window).load(function () {
 	_sectionAnchor.after(_navHtml);
 
 	// Load waypoints plugin.
-	$.getScript('res/js/waypoints.min.js', function(data, textStatus, jqxhr) {
+	/*$.getScript('res/js/waypoints.min.js', function(data, textStatus, jqxhr) {
 		// Setup waypoints.
 		$('#wiki-body h2').each(function() {
 			var subSectionName = $(this).text();
@@ -67,7 +110,7 @@ $(window).load(function () {
 				_wikiNavBar.find('li:contains(\''+subSectionName+'\')').addClass('current');
 			});
 		});
-	});
+	});*/
 
 
 	function resizeFixedNav()
