5320c7a1f8b2a19b8665d4a8f198dffb11ce19ec
Tommy McGylnn <tommymcgylnn@Tommys-MacBook-Air.local>
2012-11-20 18:10:18 -0800

    JS

--- custom.js
+++ custom.js
@@ -8,7 +8,7 @@ $(window).load(function () {
     var _navMargin = 10;
     var _sectionName = $('h1').text();
     var _navOffsetX = _wikiNavBar.offset().left;
-    var _navWidthRatio = _wikiNavBar.innerWidth() / _wikiBody.width();
+    var _navWidthRatio = _wikiNavBar.width() / _wikiBody.width();
     var _navWidthPercentage = Math.round(_navWidthRatio * 100);
 
     // Add a div after the nav bar that we will use for sizing the nav bar when it is in a fixed position.
