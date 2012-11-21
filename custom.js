911bf4705c5229f8b8bcb9801addf009ebba0e9e
Tommy McGylnn <tommymcgylnn@Tommys-MacBook-Air.local>
2012-11-20 16:36:36 -0800

    Custom JS

--- custom.js
+++ custom.js
@@ -5,7 +5,7 @@ $(window).load(function () {
     var _wikiBody = $('#wiki-body');
     var _wikiBodyTop = _wikiBody.offset().top;
     var _navMargin = 10;
-    var _sectionName = $('h1').html();
+    var _sectionName = $('h1').text();
 
     $(window).scroll(function() {
 		var scrollNavTo = Math.max(_wikiBodyTop, $(this).scrollTop() + _navMargin);
@@ -23,6 +23,6 @@ $(window).load(function () {
 		_navHtml += '<li><a href="'+link+'">'+sectionName+'</a></li>';
 	});
 	_navHtml += '</ul>';
-	_wikiNavBar.append(_navHtml);
+	_wikiNavBar.find('a:contains('+_sectionName+')').insertAfter(_navHtml);
 
 });
